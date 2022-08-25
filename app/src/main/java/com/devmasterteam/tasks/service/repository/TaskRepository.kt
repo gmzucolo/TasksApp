0bb51e0337782f494a8b7f5940e726ff1ae3433c
gmzucolo <gustavo.zucolo@hotmail.com>
2022-08-24 21:28:30 -0300

    TaskRepository created

--- app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
@@ -1,9 +1,46 @@
 package com.devmasterteam.tasks.service.repository
 
+import android.content.Context
+import com.devmasterteam.tasks.R
+import com.devmasterteam.tasks.service.constants.TaskConstants
+import com.devmasterteam.tasks.service.listener.ApiListener
+import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
 import com.devmasterteam.tasks.service.repository.remote.TaskService
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class TaskRepository {
+class TaskRepository(val context: Context) {
 
     private val taskRemoteService = RetrofitClient.getervice(TaskService::class.java)
+
+    fun create(taskModel: TaskModel, listener: ApiListener<Boolean>) {
+        val call = taskRemoteService.create(
+            taskModel.priorityId,
+            taskModel.description,
+            taskModel.dueData,
+            taskModel.complete
+        )
+        call.enqueue(object : Callback<Boolean> {
+            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
+                if (response.code() == TaskConstants.HTTP.SUCCESS) {
+                    // O objeto pego por meio do response.body é passado no método listener.onSuccess (objeto = it)
+                    response.body()?.let { listener.onSuccess(it) }
+                } else {
+                    listener.onFailure(failResponse(response.errorBody()!!.string()))
+                }
+            }
+
+            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+            }
+
+        })
+    }
+
+    private fun failResponse(string: String): String {
+        return Gson().fromJson(string, String::class.java)
+    }
 }
--- app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskFormViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskFormViewModel.kt
@@ -4,13 +4,16 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.listener.ApiListener
 import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.service.repository.PriorityRepository
+import com.devmasterteam.tasks.service.repository.TaskRepository
 
 class TaskFormViewModel(application: Application) : AndroidViewModel(application) {
 
     private val priorityRepository = PriorityRepository(application.applicationContext)
+    private val taskRepository = TaskRepository(application.applicationContext)
 
     private val _priorityList = MutableLiveData<List<PriorityModel>>()
     val priorityList: LiveData<List<PriorityModel>> = _priorityList
@@ -20,7 +23,16 @@ class TaskFormViewModel(application: Application) : AndroidViewModel(application
     }
 
     fun save(taskModel: TaskModel) {
-        TODO("Not yet implemented")
+        taskRepository.create(taskModel, object: ApiListener<Boolean>{
+            override fun onSuccess(result: Boolean) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(message: String) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
 }
